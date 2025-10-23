5611191b96102b6c8e12ab7cda2d966be30d3fdb
tristanaditya <42491208+tristanaditya@users.noreply.github.com>
2025-10-23 16:53:12 -0700

    Update udin.lua

--- udin.lua
+++ udin.lua
@@ -48,7 +48,9 @@ if game.PlaceId == 121864768012064 then
         task.wait(0)
         game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/RequestFishingMinigameStarted"):InvokeServer(1, 1)
         task.wait(0)
-end
+        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/CancelFishingInputs"):InvokeServer()
+        task.wait(0)
+    end
         
     local function cancel()
         game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/CancelFishingInputs"):InvokeServer()
